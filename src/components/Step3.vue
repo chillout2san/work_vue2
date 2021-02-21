8c7d16eb48de6fc8ba9183a5c8911bc210377b31
2san <chillout2san@gmai.com>
2021-02-21 21:19:45 +0900

    Done

---
+++ src/components/Step3.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <p class="title">STEP3</p>
+      <p class="title">ご相談内容をご記入ください</p>
+    </div>
+
+    <div>
+      <p>-ご相談内容-</p>
+      <textarea cols="50" rows="10" v-model="question"></textarea>
+    </div>
+
+    <div @click="$store.commit('displayStep3', question)">
+      <button><router-link to="/step2">前へ戻る</router-link></button>
+      <button><router-link to="/result">次へ進む</router-link></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      question: ''
+    };
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  display: inline-block;
+  margin-right: 10px;
+}
+</style>
